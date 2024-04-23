e6ef04fdc65211e6dc2039a6671b164a630d649d
XL226261 <ahazxa@syntronic.com>
2024-04-24 00:29:22 +0800

    update profile

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -10,24 +10,23 @@ function getUserProfile(userId, callback) {
                 console.error('Error executing MySQL query:', error);
                 return callback({ error: 'Internal Server Error' }, null);
             }
-            const userProfile = {
-                userId: results[0].userId,
-                roleId: results[0].roleId,
-                name: results[0].name,
-                email: results[0].email,
-                // password: results[0].password,
-                contact_no: results[0].contact_no,
-                address: results[0].address,
-                city: results[0].city,
-                state: results[0].state,
-                country: results[0].country,
-                profile_description: results[0].profile_description,
-                portfolio: results[0].portfolio,
-                website: results[0].website,
-                taggedByAdmin: results[0].taggedByAdmin,
-                adminId: results[0].adminId,
-                insert_datetime: results[0].insert_datetime
-            };
+            const userProfile = results.map(row => ({
+                userId: row.userId,
+                roleId: row.roleId,
+                name: row.name,
+                email: row.email,
+                contact_no: row.contact_no,
+                address: row.address,
+                city: row.city,
+                state: row.state,
+                country: row.country,
+                profile_description: row.profile_description,
+                portfolio: row.portfolio,
+                website: row.website,
+                taggedByAdmin: row.taggedByAdmin,
+                adminId: row.adminId,
+                insert_datetime: row.insert_datetime
+            }));
             const response = {
                 transaction: {
                     message: 'OK',
